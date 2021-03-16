7eb10ff03a50190bc1072e7cf11779f4e53e7765
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-03-15 23:10:53 -0300

    add files

---
+++ 1_starting_dotnet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace digitalinnovationone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDevezes = 10;
+
+            for (int i = 0; i < numeroDevezes; i++)
+
+            {
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
