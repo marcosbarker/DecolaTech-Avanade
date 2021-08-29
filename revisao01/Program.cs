40b9087fb01b0fa4b6f7a7e0775fad93da21e122
marcosbarker <marcos_barker@hotmail.com>
2021-08-29 18:53:19 -0300

    code: projectRevisao01

---
+++ revisao01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace revisao01
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+      string opcaoUsuario = ObterOpcaoUsuario();
+
+      while (opcaoUsuario.ToUpper() != "X") ;
+      { //ToUpper compara caixa baixa e alta
+        switch (opcaoUsuario)
+        {
+
+          case "1": //adicionar aluno
+            break;
+
+          case "2": //listar alunos   
+            break;
+
+          case "3": //calcular media
+            break;
+
+          default:
+            throw new ArgumentOutOfRangeException();
+        }
+        
+        opcaoUsuario = ObterOpcaoUsuario();
+      }
+    }
+
+    private static string ObterOpcaoUsuario()
+    {
+      Console.WriteLine("Informe a opção desejada:");
+      Console.WriteLine("1-Inserir novo aluno");
+      Console.WriteLine("2-Listar alunos");
+      Console.WriteLine("3-Calcular média geral");
+      Console.WriteLine("X-Sair");
+      Console.WriteLine();
+
+      string opcaoUsuario = Console.ReadLine();
+      return opcaoUsuario;
+    }
+  }
+}
